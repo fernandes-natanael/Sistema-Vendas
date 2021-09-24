df04b27fe2d8bb2fd05bc731b9496b5317738001
Natanael Filho <filhonatanael01@gmail.com>
2021-09-23 23:44:49 -0300

    add security for secret key

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7l&&@h=a%(=^aeck2r=0(h$j=mympl27_-!&f_lebu6qt#e8hi'
+import os
+import dotenv # <- New
+
+# Add .env variables anywhere before SECRET_KEY
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+# UPDATE secret key
+SECRET_KEY = os.environ['SECRET_KEY'] # Instead of your actual secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
